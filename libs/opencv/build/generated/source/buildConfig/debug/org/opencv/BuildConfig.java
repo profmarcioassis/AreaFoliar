661cb743592cf2b17b4b1ad24d7ffb6bd1afbeeb
Jonas <jonasmoreira001@gmail.com>
2020-04-16 08:30:45 -0300

    Commit parcial 8

--- libs/opencv/build/generated/source/buildConfig/debug/org/opencv/BuildConfig.java
+++ libs/opencv/build/generated/source/buildConfig/debug/org/opencv/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "org.opencv";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 2410;
-  public static final String VERSION_NAME = "2.4.10";
+  public static final int VERSION_CODE = 3000;
+  public static final String VERSION_NAME = "3.0.0";
 }
